f0bb9c809b5ba719c2da194eafb6718ddb4fa380
Yulia Ilyukhina <uilluhina@icloud.com>
2022-10-31 12:41:37 +0300

    edited message function

--- src/main/kotlin/Likes.kt
+++ src/main/kotlin/Likes.kt
@@ -1,10 +1,12 @@
 fun main() {
     val amountOfLikes0 = 34
     val amountOfLikes1 = 501
+    val amountOfLikes2 = 1211
     println("Понравилось " + amountOfLikes0 + " " + message(amountOfLikes0))
     println("Понравилось " + amountOfLikes1 + " " + message(amountOfLikes1))
+    println("Понравилось " + amountOfLikes2 + " " + message(amountOfLikes2))
 }
 
 fun message(amount: Int): String {
-    return if (amount % 10 == 1) "человеку" else "людям"
+    return if (amount % 10 == 1 && amount % 100 != 11) "человеку" else "людям"
 }
